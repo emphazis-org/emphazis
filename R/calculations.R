9a2bc9812483b20bbfadf872cb0fd0be3aa6ef94
luciorq <luciorqueiroz@gmail.com>
2021-03-02 16:59:34 -0300

    Added radial area calculation

--- R/calculations.R
+++ R/calculations.R
@@ -34,23 +34,23 @@ calculate_distances <- function(res_list) {
     dplyr::mutate(cumulative_distance_cm = base::cumsum(dist_cm)) %>%
     dplyr::mutate(speed = dist_cm / frame_time) %>%
     dplyr::mutate(mov_avg_speed = mov_avg(speed)) %>%
-    dplyr::mutate(count = count_area(dist_table, 50))
+    # dplyr::mutate(count_square = count_area_square(dist_table, 50)) %>%
+    dplyr::mutate(count = count_area_circle(., 50))
   return(dist_table)
 }
 
-#' Count in area
+#' Count occurrences in square area
 #'
-#' Calcutates how many times other points
+#' Calculates how many times other points
 #'   happens in an area around the main point.
 #'
 #' @param dist_table Table containing position of the mass center of the object
 #'   per frame.
 #' @param side_px Area side length measured in pixels.
 #' @export
-count_area <- function(dist_table, side_px = 50) {
+count_area_square <- function(dist_table, side_px = 50) {
   `%>%` <- dplyr::`%>%`
   count_vector <- purrr::map_int(seq_len(nrow(dist_table)), ~{
-    # for (i in seq_len(nrow(dist_table))) {
     i <- .x
     center_x <- dplyr::pull(dist_table, "x_center")[i]
     center_y <- dplyr::pull(dist_table, "y_center")[i]
@@ -72,6 +72,37 @@ count_area <- function(dist_table, side_px = 50) {
   return(count_vector)
 }
 
+#' Count occurrences in circular area
+#'
+#' Calculates how many times other points
+#'   happens in an area around the main point.
+#'
+#' @param dist_table Table containing position of the mass center of the object
+#'   per frame.
+#' @param diameter_px Area side length measured in pixels.
+#' @export
+count_area_circle <- function(dist_table, diameter_px = 50) {
+  `%>%` <- dplyr::`%>%`
+  calc_dist <- function(x1, y1, x2, y2) {
+    base::sqrt(((x1 - x2)^2) + ((y2 - y1) ^ 2))
+  }
+  count_vector <- purrr::map_int(seq_len(nrow(dist_table)), ~{
+    i <- .x
+    center_x <- dplyr::pull(dist_table, "x_center")[i]
+    center_y <- dplyr::pull(dist_table, "y_center")[i]
+    dist_vec <- purrr::map_dbl(seq_len(nrow(dist_table)), ~{
+      j <- .x
+      j_dist <- calc_dist(
+        x1 = center_x, x2 = dist_table$x_center[j],
+        y1 = center_y, y2 = dist_table$y_center[j]
+      )
+    })
+    area_count <- length(dist_vec[dist_vec <= (diameter_px/2)])
+    return(area_count)
+  })
+  return(count_vector)
+}
+
 #' Prepare path data
 #'
 #' Prepare data for path plotting.
@@ -82,7 +113,7 @@ prepare_path_data <- function(res_list) {
     .x = res_list,
     .y = seq_along(res_list),
     .f = function(res_list, frame_pos) {
-      dplyr::mutate(res_list[[1]], frame = {{ frame_pos }})
+      dplyr::mutate(res_list[[1]], frame = {{frame_pos}})
     }
   )
   return(path_df)
