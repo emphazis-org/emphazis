521234b1fde4a3f04f6dbada608bedaae3049d8b
luciorq <luciorqueiroz@gmail.com>
2021-03-06 11:09:02 -0300

    created image_tools

---
+++ R/image_tools.R
@@ -0,0 +1,47 @@
+#' Slice image
+#'
+#' @description Slice image with given X and Y coordinates.
+#'
+#' @param image_path Path to image file.
+#' @param output_path Path to save sliced image.
+#' @inheritParams proccess_video
+#' @family image_tools
+#' @export
+slice_image <- function(image_path, output_path, coord1 = NULL, coord2 = NULL) {
+
+  if (!fs::dir_exists(output_path)) {
+    fs::dir_create(output_path)
+  }
+
+  image_obj <- EBImage::readImage(image_path)
+
+  image_data <- image_obj@.Data
+
+  if (isTRUE(is.null(coord1) & is.null(coord2))) {
+    area_x_min <- 0
+    area_x_max <- dim(image_data)[1]
+    area_y_min <- 0
+    area_y_max <- dim(image_data)[2]
+
+  } else {
+    area_x_min <- coord1[2]
+    area_x_max <- coord2[1]
+    area_y_min <- coord1[2]
+    area_y_max <- coord2[2]
+  }
+
+  area_x_range <- area_x_min:area_x_max
+  area_y_range <- area_y_min:area_y_max
+
+  image_obj@.Data <- image_data[area_x_range, area_y_range, ]
+
+
+  savefile_path <- fs::path(
+    output_path,
+    stringr::str_replace(basename(image_path), ".jpg", "_slice.jpg")
+  )
+
+  EBImage::writeImage(image_obj, files = savefile_path)
+
+  return(savefile_path)
+}
---
+++ tests/testthat/test-image_tools.R
@@ -0,0 +1,58 @@
+testthat::test_that("Image slice", {
+
+  video_path <- fs::path_package("emphazis", "extdata", "sample_rec_10s.mp4")
+
+  frames_output <- emphazis::convert_video_to_image(
+    video_path = video_path,
+    frames_path = fs::path_temp("frame_1"),
+    fps = 0.2
+  )
+
+  coord1 <- c(263, 12)
+  coord2 <- c(670, 461)
+
+  image_path <- frames_output[1]
+
+  sliced_path <- slice_image(
+    image_path = image_path,
+    output_path = fs::path_temp("slice_1"),
+    coord1 = coord1,
+    coord2 = coord2
+  )
+
+  test_image <- EBImage::readImage(sliced_path)
+
+
+  testthat::expect_equal(dim(test_image@.Data), c(659, 450, 3))
+})
+
+
+testthat::test_that("Image slice data", {
+
+  video_path <- fs::path_package("emphazis", "extdata", "sample_rec_10s.mp4")
+
+  frames_vector <- emphazis::convert_video_to_image(
+    video_path = video_path,
+    frames_path = fs::path_temp("frame_1"),
+    fps = 0.2
+  )
+
+  first_frame_path <- frames_vector[1]
+
+  first_frame <- EBImage::readImage(first_frame_path)
+
+
+  frame_data <- first_frame@.Data
+
+  arena_xmax <- dim(frame_data)[1]
+  arena_ymax <- dim(frame_data)[2]
+
+  arena_frame <- first_frame
+  # 1st dim = X starting from left
+  # 2nd dim = Y starting from top
+  arena_frame@.Data <- frame_data[0:arena_xmax,0:(arena_ymax-200), ]
+  #plot(arena_frame)
+
+  testthat::expect_equal(dim(arena_frame@.Data), c(848, 280, 3))
+
+})
