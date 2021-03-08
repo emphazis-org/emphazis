0fbe6decb11321b951e8124c2bc9ef100a7218f8
luciorq <luciorqueiroz@gmail.com>
2021-03-07 21:26:05 -0300

    update dockerfile

--- data-raw/build_docker.sh
+++ data-raw/build_docker.sh
@@ -4,6 +4,6 @@ docker build . -f inst/Dockerfile -t luciorq/emphazis-app:latest
 # to run
 docker run -d \
   --restart always \
-  -p 80:3838 \
+  -p 8989:3838 \
   --name emphazis-app \
   luciorq/emphazis-app:latest
