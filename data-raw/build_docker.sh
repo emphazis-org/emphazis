65dc8ffc178c2362cb75bbd22bcb2d2c71d1fbad
luciorq <luciorqueiroz@gmail.com>
2021-03-23 20:47:29 -0300

    chore: moved build scripts to emphazis-app repo

--- data-raw/build_docker.sh
+++
@@ -1,9 +0,0 @@
-# to build
-docker build . -f inst/Dockerfile -t luciorq/emphazis-app:latest
-
-# to run
-docker run -d \
-  --restart always \
-  -p 8989:3838 \
-  --name emphazis-app \
-  luciorq/emphazis-app:latest
